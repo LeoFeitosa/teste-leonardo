7e3afa3aba9944a6d003b3e4b2e0ae8ec6ff2bbc
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-23 16:45:24 -0300

    feat: adiciona comando de fetch e job para atualização de dados da API balldontlie

---
+++ app/Console/Commands/FetchBallDontLieCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\FetchTeamsJob;
+use Illuminate\Console\Command;
+
+class FetchBallDontLieCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:ball-dont-lie';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Busca os dados da API Ball Don\'t Lie e armazena no banco ';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        FetchTeamsJob::dispatch();
+        $this->info('Job de busca de times foi despachado!');
+    }
+}
