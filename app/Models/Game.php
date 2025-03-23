8473c03a4c71f8f18734568b302be289fa3a395b
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-23 14:07:46 -0300

    feat: criar model Game

---
+++ app/Models/Game.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    /** @use HasFactory<\Database\Factories\GameFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'season',
+        'period',
+        'home_team_score',
+        'visitor_team_score',
+        'postseason',
+        'time',
+        'datetime'
+    ];
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function homeTeam()
+    {
+        return $this->belongsToMany(Team::class, 'game_team')
+            ->wherePivot('is_home_team', true);
+    }
+
+    public function visitorTeam()
+    {
+        return $this->belongsToMany(Team::class, 'game_team')
+            ->wherePivot('is_home_team', false);
+    }
+
+    public function teams()
+    {
+        return $this->homeTeam()->union($this->visitorTeam());
+    }
+
+}
