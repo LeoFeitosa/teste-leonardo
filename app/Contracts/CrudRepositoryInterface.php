3cf406b290ab8003a40e6e6b7a079b55047c16ec
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-23 16:42:35 -0300

    feat: implementar interface de repositório e repositório para gerenciamento de times

--- app/Contracts/CrudRepositoryInterface.php
+++ app/Contracts/CrudRepositoryInterface.php
@@ -1,8 +1,13 @@
 <?php
 
-namespace App\Contract;
+namespace App\Contracts;
 
 interface CrudRepositoryInterface
 {
-
+    public function create(array $data);
+    public function update($id, array $data);
+    public function delete($id);
+    public function find($id);
+    public function updateOrCreate(array $values);
+    public function all();
 }
