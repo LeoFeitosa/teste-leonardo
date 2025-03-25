bb10b16d4ce150802a2594eab32ca8c3b415dccb
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-25 12:40:15 -0300

    feat: adiciona middleware de verificação de abilities, registra alias no bootstrap e atualiza as rotas

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GamesController;
 use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('teams', TeamController::class);
-Route::apiResource('players', PlayersController::class);
-Route::apiResource('games', GamesController::class);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum', 'check.ability'])->group(function () {
+    Route::apiResource('teams', TeamController::class);
+    Route::apiResource('players', PlayersController::class);
+    Route::apiResource('games', GamesController::class);
+});
