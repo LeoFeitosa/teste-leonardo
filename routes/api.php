c5ba4f0e4cb1a425a2c2adc4602d9c214fbe38a2
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-25 13:37:14 -0300

    fix: corrigir definição das rotas de login e logout

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-
 Route::middleware(['auth:sanctum', 'check.ability'])->group(function () {
     Route::apiResource('teams', TeamController::class);
     Route::apiResource('players', PlayersController::class);
     Route::apiResource('games', GamesController::class);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
