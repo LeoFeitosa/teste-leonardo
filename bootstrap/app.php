0468e973a338a6daf228845d67094b012644e75b
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-26 11:35:48 -0300

    feat: criar e registrar middleware XAuthorization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckAbility;
+use App\Http\Middleware\XAuthorization;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(['check.ability' => CheckAbility::class]);
+        $middleware->prepend(XAuthorization::class);
+
+        $middleware->alias([
+            'check.ability' => CheckAbility::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
