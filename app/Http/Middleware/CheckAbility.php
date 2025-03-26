3bac1a43c079de1016ae17a33da519ea6c5da5cc
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2025-03-26 11:34:12 -0300

    refactor: adicionar tipo de retorno na função CheckAbility

--- app/Http/Middleware/CheckAbility.php
+++ app/Http/Middleware/CheckAbility.php
@@ -13,7 +13,7 @@ class CheckAbility
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $user = $request->user();
 
